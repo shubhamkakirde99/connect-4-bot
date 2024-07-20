5db5541c232642d01ceefff54ff81dede613f3fe
Shubham <shubham.kakirde@spit.ac.in>
2024-07-20 19:21:23 -0400

    added cron job for render

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
+import requests
 from flask import Flask
+import atexit
+from apscheduler.schedulers.background import BackgroundScheduler
+import os
 
 app = Flask(__name__)
 
+BASE_URL = os.environ.get("BASE_URL")
 
-@app.route('/')
+
+def print_date_time():
+    r = requests.get(f"{BASE_URL}/health")
+    print(r.text)
+
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=print_date_time, trigger="interval", seconds=180)
+scheduler.start()
+
+# Shut down the scheduler when exiting the app
+atexit.register(lambda: scheduler.shutdown())
+
+
+@app.route("/")
 def hello():
-    return 'Hello, World!'
+    print("[bot] - hit base url")
+    return "Hello, World!"
 
-@app.route('/bot')
-def bot():
-    return 'bot says hello'
+
+@app.route("/health")
+def health():
+    print("[bot] - hit health url")
+    return "ok"
