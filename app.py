5bc2d78e45ad0196741ea7d33d41e52b8e578dcd
Shubham <shubham.kakirde@spit.ac.in>
2024-07-29 20:23:58 -0400

    added apology, commented cron for bot

--- app.py
+++ app.py
@@ -9,17 +9,17 @@ app = Flask(__name__)
 BASE_URL = os.environ.get("BASE_URL")
 
 
-def print_date_time():
-    r = requests.get(f"{BASE_URL}/health")
-    print(r.text)
-
-
-scheduler = BackgroundScheduler()
-scheduler.add_job(func=print_date_time, trigger="interval", seconds=180)
-scheduler.start()
-
-# Shut down the scheduler when exiting the app
-atexit.register(lambda: scheduler.shutdown())
+# def print_date_time():
+#     r = requests.get(f"{BASE_URL}/health")
+#     print(r.text)
+#
+#
+# scheduler = BackgroundScheduler()
+# scheduler.add_job(func=print_date_time, trigger="interval", seconds=180)
+# scheduler.start()
+#
+# # Shut down the scheduler when exiting the app
+# atexit.register(lambda: scheduler.shutdown())
 
 
 @app.route("/")
